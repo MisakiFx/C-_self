1d1b82bb211ed4740425cb4e42282d4f676735e2
MisakiFx <1761607418@qq.com>
2018-11-05 02:10:40 +0800

    CPP语言学习笔记及项目

--- 学习代码/memset函数.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <memory.h>
-int main()
-{
-	char string[] = "Misaki";
-	printf("之前：%s",string);
-	memset(string,'*',6);//内存设置函数，第一个参数是修改的目标内存位置，第二个参数是修改为什么，第三个是修改几个,返回值是void型 
-	printf("修改后：%s",string); 
-} 
--- 学习代码/输出练习.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "英雄名称：Misaki\n";//endl可有可无 
-	cout << "伤害：56\t\t攻击距离：600"<<endl
-		 << "护甲：15.5(+3.4)\t魔抗：30(+0.0)\n"
-		 << "生命值：395(+79)\t生命回复：0.9(+0.11)"<<endl;
-}
