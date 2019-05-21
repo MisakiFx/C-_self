520dc5a018084d07c24df421460130c9f4c6d611
MisakiFx <1761607418@qq.com>
2019-05-21 23:11:19 +0800

    2019.5.21

--- 每日一题/5_20_2.cpp
+++ 每日一题/5_20_2.cpp
@@ -1,12 +1,31 @@
-//在第一个字符串中删除第二个字符串中所有字符
-//输出第一个字符串
+/*
+https://www.nowcoder.com/practice/f0db4c36573d459cae44ac90b90c6212?tpId=85&&tqId=29868&rp=1&ru=/activity/oj&qru=/ta/2017test/question-ranking
+
+
+题目描述
+输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。例如，输入”They are students.”和”aeiou”，则删除之后的第一个字符串变成”Thy r stdnts.”
+输入描述:
+每个测试输入包含2个字符串
+输出描述:
+输出删除后的字符串
+
+
+示例1
+
+输入
+They are students. aeiou
+
+输出
+Thy r stdnts.
+
+*/
 #include <iostream>
 #include <string>
 using namespace std;
 int main()
 {
   string str1, str2;
-  getline(std::cin, str1);k
+  getline(std::cin, str1);
   getline(std::cin, str2);
   string::iterator it = str2.begin();
   while(it != str2.end())
