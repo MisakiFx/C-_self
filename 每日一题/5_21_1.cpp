55bc45d4f99de8b15992b1308a8d9c2495531cba
MisakiFx <1761607418@qq.com>
2019-05-22 12:19:56 +0800

    2019.5.22

--- 每日一题/5_21_1.cpp
+++ 每日一题/5_21_1.cpp
@@ -21,56 +21,45 @@
 输出
 2
  */
-/**
 #include <iostream>
 #include <vector>
 using namespace std;
 int main()
 {
-    int num = 0;
-    cin >> num;
-    vector<int> arr;
-    arr.resize(num);
-    int count = 1;
-    for(int i = 0; i < num; i++)
-    {
-        cin >> arr[i];
-    }
-    for(int i = 1; i < num - 1; i++)
-    {
-        if ((arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) || (arr[i - 1] > arr[i] && arr[i] < arr[i + 1]))
-        {
-            count++;
-            if(i != num - 3)
-            i++;
-            
-        }
-    }
-    cout << count << endl;
-}
-*/
-#include <iostream>
-#include <vector>
-using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    vector<int> v;
-    v.resize(n);
-    for (int i = 0; i < n; ++i)
-        cin >> v[i];
-    int ret = 1;
-    for (int i = 1; i < n - 1; i++)
-    {
-        //找出波峰波谷
-        if ((v[i - 1] < v[i] && v[i] > v[i + 1]) || (v[i - 1] > v[i] && v[i] < v[i + 1]))
-        {
-            ret++;
-            // 这里对应处理的就是上面情况二描述的问题
-            if(i != n - 3)
-            i++;
-        }
-    }
-    cout << ret << endl;
+ int n;
+ cin >> n;
+ // 注意这里多给了一个值，是处理越界的情况的比较，具体参考上面的解题思路
+ vector<int> a;
+ a.resize(n + 1);
+ a[n] = 0;
+ //读入数组
+ int i = 0;
+ for (i = 0; i < n; ++i)
+ cin >> a[i];
+ i = 0;
+ int count = 0;
+ while (i < n)
+ {
+ // 非递减子序列
+ if (a[i] < a[i + 1])
+ {
+ while (i < n && a[i] <= a[i + 1])
+ i++;
+ count++;
+ i++;
+ }
+ else if (a[i] == a[i + 1])
+ {
+ i++;
+ }
+ else // 非递增子序列
+ {
+ while (i < n && a[i] >= a[i + 1])
+ i++;
+ count++;
+ i++;
+ }
+ }
+ cout << count << endl;
+ return 0;
 }
