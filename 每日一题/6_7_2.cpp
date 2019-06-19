61c9009caf33f62fa2f4e454f1de1b98c837e1e7
MisakiFx <1761607418@qq.com>
2019-06-20 02:03:11 +0800

    2019.6.20

--- 每日一题/6_7_2.cpp
+++ 每日一题/6_7_2.cpp
@@ -1,3 +1,22 @@
+//https://www.nowcoder.com/practice/5821836e0ec140c1aa29510fd05f45fc?tpId=37&&tqId=21301&rp=1&ru=/
+/**
+ * 题目描述
+请设计一个算法完成两个超长正整数的加法。
+
+输入描述:
+输入两个字符串数字
+
+输出描述:
+输出相加后的结果，string型
+
+示例1
+输入
+99999999999999999999999999999999999999999999999999
+1
+
+输出
+100000000000000000000000000000000000000000000000000
+ */
 #include <iostream>
 #include <string>
 #include <stack>
@@ -15,6 +34,7 @@ int main()
         string& shortNum = num1.size() <= num2.size() ? num1 : num2;
         bool carryFlag = false;
         //cout << len << " " << len2 << endl;
+        //将两个字符串进行逐位相加，并且标记进位，直到短字符串结束为止
         for(int i = len2 - 1, j = len - 1; j >= 0; i--, j--)
         {
             int curRes;
@@ -34,6 +54,7 @@ int main()
             }
             res.push('0' + curRes);
         }
+        //将长字符串多余的部分也压入结果栈，同时标记进位
         for(int i = len2 - len - 1; i >= 0; i--)
         {
             int curRes;
@@ -53,10 +74,12 @@ int main()
             }
             res.push('0' + curRes);
         }
+        //最后如果还标记有进位则需要在开头压入1
         if(carryFlag == true)
         {
             res.push('1');
         }
+        //输出
         while(!res.empty())
         {
             cout << res.top();
@@ -65,3 +88,47 @@ int main()
         cout << endl;
     }
 }
+/**
+ * #include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string addStrings(string num1, string num2) {
+//由低位向高位相加
+int i = num1.size() - 1;
+int j = num2.size() - 1;
+string result = "";
+//当前位的相加结果
+int carry = 0;
+while (i >= 0 || j >= 0)
+{ if (i >= 0) {
+carry += num1[i] - '0';
+}
+if (j >= 0) {
+carry += num2[j] - '0';
+}
+//当前为的最大值不大于10
+result += (char)(carry % 10 + '0');
+//如果大于10,向上进一位
+carry /= 10;
+i--;
+j--;
+}
+//相加完之后，如果还有进位，则再加1
+if (carry == 1)
+{ result += '1';
+}
+//整体逆置
+reverse(result.begin(), result.end());
+return result;
+}
+int main()
+{
+string s1, s2;
+while(cin>>s1>>s2)
+{
+cout<<addStrings(s1, s2)<<endl;
+}
+return 0;
+}
+ */
