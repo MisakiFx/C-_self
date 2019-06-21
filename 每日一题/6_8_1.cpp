e5848b582ef75d924d362e5b87577491bce0edf7
MisakiFx <1761607418@qq.com>
2019-06-21 16:52:20 +0800

    2019.6.21

--- 每日一题/6_8_1.cpp
+++ 每日一题/6_8_1.cpp
@@ -1,4 +1,19 @@
 //兄弟你让我求斐波那契我能不会么，你让我求尼玛兔子啊！！！！
+//https://www.nowcoder.com/practice/1221ec77125d4370833fd3ad5ba72395?tpId=37&amp;&amp;tqId=21260&amp;rp=1&amp;ru
+/**
+ * 题目描述
+有一只兔子，从出生后第3个月起每个月都生一只兔子，小兔子长到第三个月后每个月又生一只兔子，假如兔子都不死，问每个月的兔子总数为多少？
+输入描述:
+输入int型表示month
+
+输出描述:
+输出兔子总数int型
+示例1
+输入
+9
+输出
+34
+ */
 #include <iostream>
 #include <vector>
 using namespace std;
--- 每日一题/6_8_2.cpp
+++ 每日一题/6_8_2.cpp
@@ -1,3 +1,28 @@
+//https://www.nowcoder.com/practice/43072d50a6eb44d2a6c816a283b02036?tpId=37&amp;&amp;tqId=21294&amp;rp=1&amp;r
+/**
+ * 
+输入：
+通配符表达式；
+一组字符串。
+
+输出：
+返回匹配的结果，正确输出true，错误输出false
+
+输入描述:
+先输入一个带有通配符的字符串，再输入一个需要匹配的字符串
+
+输出描述:
+返回匹配的结果，正确输出true，错误输出false
+
+示例1
+输入
+te?t*.*
+txt12.xls
+
+输出
+false
+ */
+/*
 #include <iostream>
 #include <string>
 using namespace std;
@@ -69,6 +94,51 @@ int main()
             cout << "true" << endl;
         }
         else
+        {
+            cout << "false" << endl;
+        }
+    }
+}
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+bool Compare(const char* str1, const char* str2)
+{
+    if(*str1 == '\0' && *str2 == '\0')
+    {
+        return true;
+    }
+    if(*str1 == '\0' || *str2 == '\0')
+    {
+        return false;
+    }
+    if(*str1 == *str2)
+    {
+        return Compare(str1 + 1, str2 + 1);
+    }
+    else if(*str1 == '?')
+    {
+        return Compare(str1 + 1, str2 + 1);
+    }
+    else if(*str1 == '*')
+    {
+        return (Compare(str1 + 1, str2) || Compare(str1 + 1, str2 + 1) || Compare(str1, str2 + 1));
+    }
+    return false;
+}
+int main()
+{
+    string str1;
+    string str2;
+    while(cin >> str1 >> str2)
+    {
+        bool res = Compare(str1.c_str(), str2.c_str());
+        if(res == true)
+        {
+            cout << "true" << endl;
+        }
+        else if(res == false)
         {
             cout << "false" << endl;
         }
