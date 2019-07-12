ddbb4b36ecbcdc159083e4782b2cca16eac14fc9
MisakiFx <1761607418@qq.com>
2019-07-12 16:33:39 +0800

    2019.7.12

--- 每日一题/6_13_2.cpp
+++ 每日一题/6_13_2.cpp
@@ -1,3 +1,21 @@
+//https://www.nowcoder.com/practice/e896d0f82f1246a3aa7b232ce38029d4?tpId=37&&tqId=21282&rp=1&ru=/activity/oj&qru=/ta/huawei/question-ranking
+/**
+ * 题目描述
+找出字符串中第一个只出现一次的字符
+
+输入描述:
+输入一个非空字符串
+
+输出描述:
+输出第一个只出现一次的字符，如果不存在输出-1
+
+示例1
+输入
+asdfasdfo
+输出
+o
+ */
+/* 
 #include <iostream>
 #include <string>
 using namespace std;
@@ -29,5 +47,36 @@ int main()
         }
         if(i == str.size())
         cout << "-1" << endl;
+    }
+}
+*/
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main()
+{
+    string str;
+    while(cin >> str)
+    {
+        map<char, int> count;
+        for(int i = 0; i < str.size(); i++)
+        {
+            count[str[i]]++;
+        }
+        int i;
+        //这里要用字符串进行遍历才能找到第一个出现的
+        for(i = 0; i < str.size(); i++)
+        {
+            if(count[str[i]] == 1)
+            {
+                cout << str[i] << endl;
+                break;
+            }
+        }
+        if(i == str.size())
+        {
+            cout << -1 << endl;
+        }
     }
 }
