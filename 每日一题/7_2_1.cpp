9dd191bf8379ae0b04dc65b077435cbb275b0a8a
MisakiFx <1761607418@qq.com>
2019-07-15 22:26:55 +0800

    2019.7.15

--- 每日一题/7_2_1.cpp
+++ 每日一题/7_2_1.cpp
@@ -1,21 +1,33 @@
+//https://www.nowcoder.com/questionTerminal/22243d016f6b47f2a6928b4313c85387
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
 #include <iostream>
 #include <vector>
 using namespace std;
 
+//class Solution {
+//public:
+//    int jumpFloorII(int number) {
+//        vector<int> steps(number + 1, 0);
+//        steps[0] = 1;
+//        for(int i = 1; i < steps.size(); i++)
+//        {
+//            steps[i] = 0;
+//            for(int j = i - 1; j >= 0; j--)
+//            {
+//                steps[i] += steps[j];
+//            }
+//        }
+//        return steps[number];
+//    }
+//};
 class Solution {
 public:
     int jumpFloorII(int number) {
-        vector<int> steps(number + 1, 0);
-        steps[0] = 1;
-        for(int i = 1; i < steps.size(); i++)
-        {
-            steps[i] = 0;
-            for(int j = i - 1; j >= 0; j--)
-            {
-                steps[i] += steps[j];
-            }
-        }
-        return steps[number];
+        //根据类推公式得
+        //n级台阶就有2 ^ (n - 1)种跳法
+        return 1 << (number - 1);
     }
 };
 int main()
