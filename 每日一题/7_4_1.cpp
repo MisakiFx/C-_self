bb76aae76b5f05b8f66ae3aa92349b255511239e
MisakiFx <1761607418@qq.com>
2019-08-01 00:47:14 +0800

    2019.8.1

--- 每日一题/7_4_1.cpp
+++ 每日一题/7_4_1.cpp
@@ -1,4 +1,13 @@
 //但是傻逼牛客不给过
+//这题就是个bug
+//https://www.nowcoder.com/questionTerminal/b89b14a3b5a94e438b518311c5156366
+/**
+ * 链接：https://www.nowcoder.com/questionTerminal/b89b14a3b5a94e438b518311c5156366
+来源：牛客网
+
+给定一个长度不小于2的数组arr。 写一个函数调整arr，使arr中要么所有的偶数位上都是偶数，要么所有的奇数位上都是奇数上。 要求：如果数组长度为N，时间复杂度请达到O(N)，
+额外空间复杂度请达到O(1),下标0,2,4,6...算作偶数位,下标1,3,5,7...算作奇数位，例如[1,2,3,4]调整为[2,1,4,3]即可
+ */
 #include <iostream>
 #include <vector>
 using namespace std;
