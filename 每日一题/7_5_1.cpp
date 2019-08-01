92554110a99fe4924d74bb0b5aef1c55675d0948
MisakiFx <1761607418@qq.com>
2019-08-01 16:35:25 +0800

    2019.8.1

--- 每日一题/7_5_1.cpp
+++ 每日一题/7_5_1.cpp
@@ -1,8 +1,56 @@
 //动态规划背包问题
+//https://www.nowcoder.com/questionTerminal/296c2c18037843a7b719cf4c9c0144e4
+/**
+ * 链接：https://www.nowcoder.com/questionTerminal/296c2c18037843a7b719cf4c9c0144e4
+来源：牛客网
+
+给定一个全是正数的数组arr，定义一下arr的最小不可组成和的概念： 1，arr的所有非空子集中，把每个子集内的所有元素加起来会出现很多的值，其中最小的记为min，最大的记为max； 2，在区间[min,max]上，如果有一些正数不可以被arr某一个子集相加得到，那么这些正数中最小的那个，就是arr的最小不可组成和； 3，在区间[min,max]上，如果所有的数都可以被arr的某一个子集相加得到，那么max+1是arr的最小不可组成和； 举例： arr = {3,2,5} arr的min为2，max为10，在区间[2,10]上，4是不能被任何一个子集相加得到的值中最小的，所以4是arr的最小不可组成和； arr = {3,2,4} arr的min为2，max为9，在区间[2,9]上，8是不能被任何一个子集相加得到的值中最小的，所以8是arr的最小不可组成和； arr = {3,1,2} arr的min为1，max为6，在区间[2,6]上，任何数都可以被某一个子集相加得到，所以7是arr的最小不可组成和； 请写函数返回arr的最小不可组成和。
+ */
 #include <iostream>
 #include <vector>
 #include <climits>
 using namespace std;
+//class Solution {
+//public:
+//	/**
+//	 *	正数数组中的最小不可组成和
+//	 *	输入：正数数组arr
+//	 *	返回：正数数组中的最小不可组成和
+//	 */
+//	int getFirstUnFormedNum(vector<int> arr, int len) 
+//    {
+//        if (arr.size() == 0) 
+//        {
+//            return 1;
+//        }
+//        int min = INT_MAX;
+//        int max = 0;
+//        for (int i = 0;i < arr.size();i++) 
+//        {
+//            min = std::min(min, arr[i]);
+//            max += arr[i];
+//        }
+//        vector<bool> dp;
+//        dp.resize(max + 1);
+//        dp[0] = true;
+//        dp[arr[0]] = true;
+//        for(int i = 1;i < arr.size(); i++) 
+//        {
+//            for(int col = dp.size() - 1; col-arr[i] >= 0; col--) 
+//            {
+//                dp[col] = dp[col - arr[i]] ? true : dp[col];
+//            }
+//        }
+//        for (int num = min + 1; num <= max; num++) 
+//        {
+//            if(!dp[num]) 
+//            {
+//                return num;
+//            }
+//        }
+//        return max + 1;
+//    }
+//};
 class Solution {
 public:
 	/**
@@ -10,35 +58,36 @@ public:
 	 *	输入：正数数组arr
 	 *	返回：正数数组中的最小不可组成和
 	 */
-	int getFirstUnFormedNum(vector<int> arr, int len) 
-    {
-        if (arr.size() == 0) 
+	int getFirstUnFormedNum(vector<int> arr, int len) {
+        int min = arr[0];
+        int max = arr[0];
+        for(int i = 1; i < arr.size(); i++)
         {
-            return 1;
-        }
-        int min = INT_MAX;
-        int max = 0;
-        for (int i = 0;i < arr.size();i++) 
-        {
-            min = std::min(min, arr[i]);
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }
             max += arr[i];
         }
-        vector<bool> dp;
-        dp.resize(max + 1);
-        dp[0] = true;
-        dp[arr[0]] = true;
-        for(int i = 1;i < arr.size(); i++) 
+        //动态规划求解背包问题
+        //dp[i][j]表示数组前i个数的情况下，在min-max区间中这个数可以由数组中
+        //任意数组合出的最大的数
+        vector<int> dp(max + 1, 0);
+        for(int i = 0; i < len; i++)
         {
-            for(int col = dp.size() - 1; col-arr[i] >= 0; col--) 
+            for(int j = max; j >= arr[i]; j--)
             {
-                dp[col] = dp[col - arr[i]] ? true : dp[col];
+                if(dp[j] < dp[j - arr[i]] + arr[i])
+                {
+                    dp[j] = dp[j - arr[i]] + arr[i];
+                }
             }
         }
-        for (int num = min + 1; num <= max; num++) 
+        for(int i = min; i <= max; i++)
         {
-            if(!dp[num]) 
+            if(dp[i] != i)
             {
-                return num;
+                return i;
             }
         }
         return max + 1;
--- 每日一题/7_5_2.cpp
+++ 每日一题/7_5_2.cpp
@@ -1,20 +1,63 @@
 //数学递推的递推公式，或者找规律的答案
+//https://www.nowcoder.com/questionTerminal/1d18c0841e64454cbc3afaea05e2f63c
+/**
+*
+居然有假币！ 现在猪肉涨了，但是农民的工资却不见涨啊，没钱怎么买猪肉啊。nowcoder这就去买猪肉，结果找来的零钱中有假币！！！可惜nowcoder 一不小心把它混进了一堆真币里面去了。只知道假币的重量比真币的质量要轻，给你一个天平（天平两端能容纳无限个硬币），请用最快的时间把那个可恶的假币找出来。
+
+输入描述:
+1≤n≤2^30,输入0结束程序。
+
+输出描述:
+最多要称几次一定能把那个假币找出来？
+示例1
+输入
+3
+12
+0
+输出
+1
+3
+ */
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    while(cin >> n)
+//    {
+//        if(n == 0)
+//        {
+//            break;
+//        }
+//        int count = 0;
+//        uint64_t sum = 1;
+//        while(sum < n)
+//        {
+//            sum *= 3;
+//            count++;
+//        }
+//        cout << count << endl;
+//    }
+//}
+// write your code here cpp
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n;
-    while(cin >> n)
+    long long num;
+    while(cin >> num && num != 0)
     {
-        if(n == 0)
-        {
-            break;
-        }
         int count = 0;
-        uint64_t sum = 1;
-        while(sum < n)
+        while(num >= 2)
         {
-            sum *= 3;
+            if(num % 3 != 0)
+            {
+                num = num / 3 + 1;
+            }
+            else
+            {
+                num = num / 3;
+            }
             count++;
         }
         cout << count << endl;
