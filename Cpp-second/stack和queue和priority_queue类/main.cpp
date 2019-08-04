814faa75e1fb9f857d638be6322fcfac07a18183
MisakiFx <1761607418@qq.com>
2019-08-04 19:17:12 +0800

    2019.8.4

--- Cpp-second/stack和queue和priority_queue类/main.cpp
+++ Cpp-second/stack和queue和priority_queue类/main.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include <iostream>     // std::cout
+#include <queue>
+#include <deque>
+#include <functional>
 using namespace std;
-int main()
+int main () 
 {
-
+    priority_queue<int, vector<int>, less<int>> que(less<int>(), vector<int>(3, 0));
 }
