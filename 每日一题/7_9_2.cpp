b067e5e971b7ee0683a6e3b6686b689f71c5e165
MisakiFx <1761607418@qq.com>
2019-08-10 19:20:44 +0800

    2019.8.10

--- 每日一题/7_9_2.cpp
+++ 每日一题/7_9_2.cpp
@@ -1,3 +1,32 @@
+//https://www.nowcoder.com/questionTerminal/c0a4b917a15f40a49ca10532ab9019fb
+/**
+ * 链接：https://www.nowcoder.com/questionTerminal/c0a4b917a15f40a49ca10532ab9019fb
+来源：牛客网
+
+NowCoder号称自己已经记住了1-100000之间所有的斐波那契数。
+为了考验他，我们随便出一个数n，让他说出第n个斐波那契数。当然，斐波那契数会很大。因此，如果第n个斐波那契数不到6位，则说出该数；否则只说出最后6位。
+
+输入描述:
+输入有多组数据。
+每组数据一行，包含一个整数n (1≤n≤100000)。
+
+
+输出描述:
+对应每一组输入，输出第n个斐波那契数的最后6位。
+示例1
+输入
+1
+2
+3
+4
+100000
+输出
+1
+2
+3
+5
+537501
+ */
 #include <iostream>
 using namespace std;
 int main()
