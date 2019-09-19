bc9f887c9b9afc19fe10c399e843db63207e9763
Misaki <1761607418@qq.com>
2019-09-20 01:33:17 +0800

    2019.9.20

--- Cpp-second/多态/mian.cpp
+++ Cpp-second/多态/mian.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
-class Person
+class Abstract
+{
+public:
+    virtual void BuyTicket() = 0;
+    virtual void Ticket() = 0;
+};
+class Person : public Abstract
 {
 public:
     virtual void BuyTicket()
     {
         cout << "full ticket!" << endl;
+    }
+    virtual void Ticket()
+    {
+
     }
     virtual ~Person()
     {
@@ -13,11 +23,15 @@ public:
     }
 };
 //Student类继承于
-class Student: public Person
+class Student: public Abstract
 {
     virtual void BuyTicket()
     {
         cout << "student ticket!" << endl;
+    }
+    virtual void Ticket()
+    {
+
     }
     virtual ~Student()
     {
@@ -27,9 +41,9 @@ class Student: public Person
 int main()
 {
     //这里肯定会造成内存泄露，这里为了演示暂时不考虑内存泄露的问题
-    Person* p = new Person();
+    Abstract* p = new Person();
     p->BuyTicket();
-    Person* p2 = new Student();//切割，将子类对象指针赋值给父类指针
+    Abstract* p2 = new Student();//切割，将子类对象指针赋值给父类指针
     p2->BuyTicket();
     delete p;//调用Person析构函数
     delete p2;//调用Student析构函数，由于Student继承于Person所以还会再调用基类析构函数
