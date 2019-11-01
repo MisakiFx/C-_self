d2cfef91245fe59fd6aa20821107a200831d1a94
Misaki <1761607418@qq.com>
2019-11-02 02:18:43 +0800

    2019.11.2

---
+++ 剑指offer/10_斐波那契数列.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int Fibonacci_digui(int n)
+    {
+        if(n < 2)
+        {
+            return n;
+        }
+        return Fibonacci_digui(n - 1) + Fibonacci_digui(n - 2);
+    }
+    //迭代解法
+    int Fibonacci(int n) {
+        if(n < 2)
+        {
+            int arr[2] = {0, 1};
+            return arr[n];
+        }
+        long long fib1 = 0, fib2 = 1, fibn = 0;
+        for(int i = 2; i <= n; i++)
+        {
+            fibn = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = fibn;
+        }
+        return fibn;
+    }
+};
---
+++ 剑指offer/8_二叉树的下一个结点.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+struct TreeLinkNode {
+    int val;
+    struct TreeLinkNode *left;
+    struct TreeLinkNode *right;
+    struct TreeLinkNode *next;
+    TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+    }
+};
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        if(pNode == nullptr)
+        {
+            return nullptr;
+        }
+        //当前节点右子树不为空找右子树的最左节点
+        if(pNode->right != nullptr)
+        {
+            pNode = pNode->right;
+            while(pNode->left)
+            {
+                pNode = pNode->left;
+            }
+        }
+        else
+        {
+            TreeLinkNode* parent = pNode->next;
+            while(parent != nullptr && pNode == parent->right)
+            {
+                pNode = parent;
+                parent = parent->next;
+            }
+            pNode = parent;
+        }
+        return pNode;
+    }
+};
