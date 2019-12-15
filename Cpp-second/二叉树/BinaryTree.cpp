e88b9b009ecb752d4fba38c8565ec1507a62f2b1
Misaki <1761607418@qq.com>
2019-12-15 19:14:59 +0800

    2019.12.15

---
+++ Cpp-second/二叉树/BinaryTree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+//二叉链结构
+template<class T>
+struct BinaryTreeNode
+{
+    T _data;
+    BinaryTreeNode<T>* _left;
+    BinaryTreeNode<T>* _right;
+};
+
+template<class T>
+class BinaryTree
+{
+public:
+    BinaryTree()
+        :_head(nullptr)
+        ,_size(0)
+    {}
+
+private:
+    BinaryTree<T>* _head;           //与链表一样存储一个头结点即可
+    size_t _size;                   //节点个数
+};
