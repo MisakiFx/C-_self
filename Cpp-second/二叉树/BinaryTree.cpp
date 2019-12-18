79b3da8a78c09a96e9ed06d05bcce1fba3313410
Misaki <1761607418@qq.com>
2019-12-18 23:32:03 +0800

    2019.12.18

--- Cpp-second/二叉树/BinaryTree.cpp
+++ Cpp-second/二叉树/BinaryTree.cpp
@@ -4,6 +4,11 @@
 template<class T>
 struct BinaryTreeNode
 {
+    BinaryTreeNode(const T& data = T())
+        :_data(data)
+        ,_left(nullptr)
+        ,_right(nullptr)
+    {}
     T _data;
     BinaryTreeNode<T>* _left;
     BinaryTreeNode<T>* _right;
@@ -16,11 +21,27 @@ public:
     BinaryTree()
         :_head(nullptr)
         ,_size(0)
+    {}
+    BinaryTree(const std::string& preOrder)
     {
-
+        _head = CreateBPreOrder(preOrder, 0);
     }
-    
 private:
-    BinaryTree<T>* _head;           //与链表一样存储一个头结点即可
+    BinaryTreeNode<T> CreateBPreOrder(const std::string& preOrder, int i)
+    {
+        if(preOrder[i] != '#')
+        {
+            BinaryTreeNode<T>* root = new BinaryTreeNode<T>(preOrder[i] - '0');
+            root->_left = CreateBPreOrder(preOrder, ++i);
+            root->_right = CreateBPreOrder(preOrder, ++i);
+            return root;
+        }
+        else 
+        {
+            return nullptr;
+        }
+    }
+private:
+    BinaryTreeNode<T>* _head;           //与链表一样存储一个头结点即可
     size_t _size;                   //节点个数
 };
