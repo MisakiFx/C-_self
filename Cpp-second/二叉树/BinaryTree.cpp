5fb5da996c1a2439935f20cd346efc7108063581
Misaki <1761607418@qq.com>
2020-01-06 17:48:05 +0800

    2020.1.6

--- Cpp-second/二叉树/BinaryTree.cpp
+++ Cpp-second/二叉树/BinaryTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 //二叉链结构
 template<class T>
@@ -25,14 +26,90 @@ public:
     BinaryTree(const std::string& preOrder)
     {
         int i = 0;
-        _head = CreateBPreOrder(preOrder, i);
+        _head = CreateByPreOrder(preOrder, i);
     }
     //前序遍历
     void PreOrder()
     {
         PreOrderCore(_head);
     }
+    //中序遍历
+    void InOrder()
+    {
+        InOrderCore(_head);
+    }
+    //后序遍历
+    void PostOrder()
+    {
+        PostOrderCore(_head);
+    }
+    //层序遍历
+    void LevelOrder()
+    {
+        std::queue<BinaryTreeNode<T>*> que;
+        que.push(_head);
+        while(!que.empty())
+        {
+            BinaryTreeNode<T>*temp = que.front();
+            que.pop();
+            if(temp == nullptr)
+            {
+                std::cout << "#";
+            }
+            else 
+            {
+                std::cout << (char)(temp->_data + 'A');
+                que.push(temp->_left);
+                que.push(temp->_right);
+            }
+        }
+    }
+    //判断是不是一棵完全二叉树
+    //层序遍历二叉树，遍历到第一个nullptr结点，
+    //就遍历整个队列，如果全为空则是完全二叉树
+    //否则就不是
+    bool IsComplateTree()
+    {
+        std::queue<BinaryTreeNode<T>*> que;
+        que.push(_head);
+        while(!que.empty())
+        {
+            BinaryTreeNode<T>* temp = que.front();
+            que.pop();
+            if(temp == nullptr)
+            {
+                break;
+            }
+            else
+            {
+                que.push(temp->_left);
+                que.push(temp->_right);
+            }
+        }
+        while(!que.empty())
+        {
+
+            BinaryTreeNode<T>* temp = que.front();
+            que.pop();
+            if(temp != nullptr)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 private:
+    void InOrderCore(BinaryTreeNode<T>* root)
+    {
+        if(root == nullptr)
+        {
+            std::cout << "#";
+            return;
+        }
+        InOrderCore(root->_left);
+        std::cout << (char)(root->_data + 'A');
+        InOrderCore(root->_right);
+    }
     void PreOrderCore(BinaryTreeNode<T>* root)
     {
         if(root == nullptr)
@@ -44,13 +121,24 @@ private:
         PreOrderCore(root->_left);
         PreOrderCore(root->_right);
     }
-    BinaryTreeNode<T>* CreateBPreOrder(const std::string& preOrder, int& i)
+    void PostOrderCore(BinaryTreeNode<T>* root)
+    {
+        if(root == nullptr)
+        {
+            std::cout << "#";
+            return;
+        }
+        PostOrderCore(root->_left);
+        PostOrderCore(root->_right);
+        std::cout << (char)('A' + root->_data);
+    }
+    BinaryTreeNode<T>* CreateByPreOrder(const std::string& preOrder, int& i)
     {
         if(preOrder[i] != '#')
         {
             BinaryTreeNode<T>* root = new BinaryTreeNode<T>(preOrder[i] - 'A');
-            root->_left = CreateBPreOrder(preOrder, ++i);
-            root->_right = CreateBPreOrder(preOrder, ++i);
+            root->_left = CreateByPreOrder(preOrder, ++i);
+            root->_right = CreateByPreOrder(preOrder, ++i);
             return root;
         }
         else 
@@ -66,6 +154,14 @@ private:
 
 int main()
 {
-    BinaryTree<int> tree("ABD##E#H##CF##G##");
-    //tree.PreOrder();
+    BinaryTree<int> tree("ABD##E##CF##G##");
+    tree.PreOrder();
+    std::cout << std::endl;
+    tree.InOrder();
+    std::cout << std::endl;
+    tree.PostOrder();
+    std::cout << std::endl;
+    tree.LevelOrder();
+    std::cout << std::endl;
+    std::cout << "Is Complate Tree: " << tree.IsComplateTree() << std::endl;;
 }
