3bf81d905969cdb8f75ec51cecc73e45fe86c694
Misaki <1761607418@qq.com>
2020-02-19 00:05:17 +0800

    2020.2.19

---
+++ 剑指offer/55_二叉树的深度.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+class Solution {
+public:
+    int TreeDepth(TreeNode* pRoot)
+    {
+        if(pRoot == nullptr)
+        {
+            return 0;
+        }
+        int left = TreeDepth(pRoot->left);
+        int right = TreeDepth(pRoot->right);
+        return max(left, right) + 1;
+    }
+};
