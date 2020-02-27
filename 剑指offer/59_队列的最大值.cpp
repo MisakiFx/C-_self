8da8f1fe84e85d7d56383945b7bf8981ed001f4e
Misaki <1761607418@qq.com>
2020-02-28 01:41:51 +0800

    2020.2.28

--- 剑指offer/59_队列的最大值.cpp
+++ 剑指offer/59_队列的最大值.cpp
@@ -1,40 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <deque>
-using namespace std;
+#include <assert.h>
 
-class Solution {
+template <class T>
+class Queue
+{
 public:
-    vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+    Queue()
+        :_curIndex(0)
+    {}
+    const T& GetMax()
     {
-        vector<int> res;
-        if(num.size() < size || size <= 0)
-        {
-            return res;
-        }
-        deque<int> index;
-        for(int i = 0; i < size; i++)
-        {
-            while(!index.empty() && num[index.back()] <= num[i])
-            {
-                index.pop_back();
-            }
-            index.push_back(i);
-        }
-        for(int i = size; i < num.size(); i++)
-        {
-            res.push_back(num[index.front()]);
-            while(!index.empty() && num[index.back()] <= num[i])
-            {
-                index.pop_back();
-            }
-            if(!index.empty() && ((i - index.front()) >= (int)(size)))
-            {
-                index.pop_front();
-            }
-            index.push_back(i);
-        }
-        res.push_back(num[index.front()]);
-        return res;
+        assert(!_maxQue.empty());
+        return _maxQue.front()._data;
     }
+    void Push_Back(const T& data)
+    {
+        while(!_maxQue.empty() && data >= _maxQue.back()._data)
+        {
+            _maxQue.pop_back();
+        }
+        IndexQue tempIndex(data, _curIndex);
+        _maxQue.push_back(tempIndex);
+        _queue.push_back(tempIndex);
+        _curIndex++;
+    }
+    void Pop_Front()
+    {
+        assert(!_queue.empty() && !_maxQue.empty());
+        if(_queue.front()._index == _maxQue.front()._index)
+        {
+            _maxQue.pop_front();
+        }
+        _queue.pop_front();
+    }
+private:
+    struct IndexQue
+    {
+        IndexQue(const T& data, int index)
+            :_data(data)
+            ,_index(index)
+        {}
+        T _data;
+        int _index;
+    };
+    std::deque<IndexQue> _queue;
+    std::deque<IndexQue> _maxQue;
+    unsigned int _curIndex;
 };
+int main()
+{
+    Queue<int> que;
+    que.Push_Back(6);
+    que.Push_Back(4);
+    que.Push_Back(3);
+    que.Push_Back(6);
+    que.Pop_Front();
+    std::cout << que.GetMax() << std::endl;
+}
+
